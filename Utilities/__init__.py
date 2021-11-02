8189353efd7111ae63a340591a50019e62174c79
Macavitymadcap <danielthekiernan@gmail.com>
2021-11-02 12:47:41 +0000

    added search_monsters to init

--- Utilities/__init__.py
+++ Utilities/__init__.py
@@ -4,3 +4,4 @@ from Utilities.utilities import format_mod
 from Utilities.utilities import format_num
 from Utilities.utilities import format_text
 from Utilities.utilities import name_stringer
+from Utilities.utilities import search_monsters
