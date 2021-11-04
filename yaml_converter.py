423d8663c6b49e8c852ff219787a316c7dd5021b
Macavitymadcap <danielthekiernan@gmail.com>
2021-11-04 21:58:49 +0000

    returned indent to dumps function

--- yaml_converter.py
+++ yaml_converter.py
@@ -3,4 +3,4 @@ import yaml
 
 with open('MonsterManual.yaml', 'r') as yaml_file:
     yaml_monsters = yaml.safe_load(yaml_file)
-json_monsters = json.dumps(yaml_monsters)
+json_monsters = json.dumps(yaml_monsters, indent=4)
